bd671b7ee011b72af344d2e31627dbbfe9324c86
Keith <kthohr@users.noreply.github.com>
2015-02-04 19:28:23 -0500

    Version 0.4.3

---
+++ R/BVARM.R
@@ -0,0 +1,222 @@
+# 01/12/2015
+BVARM.default <- function(mydata,coefprior=NULL,p=4,constant=TRUE,irf.periods=20,keep=10000,burnin=1000,VType=1,decay="H",HP1=0.5,HP2=0.5,HP3=1,HP4=2){
+  #
+  kerr <- .bvarmerrors(mydata,p,coefprior,constant,VType,decay,HP4)
+  #
+  kdata <- .bvarmdata(mydata,p,constant)
+  #
+  kprior <- .bvarmprior(kdata$Y,kdata$X,p,kdata$M,kdata$K,kdata$Yraw,kdata$Tp,kerr$coefprior,constant,VType,decay,HP1,HP2,HP3,HP4)
+  #
+  kreps <- .bvarmreplications(kdata$Y,kdata$Z,kprior$aPr,kprior$alpha,kprior$BVPr,kprior$Sigma,kdata$K,kdata$M,p,constant,keep,burnin,irf.periods)
+  #
+  bvarmret <- list(IRFs=kreps$IRFs,BetaVPr=kprior$BVPr,Beta=kreps$Beta,Sigma=kreps$Sigma,BDraws=kreps$BetaDraws,data=mydata,constant=constant)
+  class(bvarmret) <- "BVARM"
+  return(bvarmret)
+  #
+}
+
+.bvarmerrors <- function(mydata,p,coefprior,constant,VType,decay,HP4){
+  #
+  if(ncol(mydata)<2){
+    stop("need more than 1 variable.\n",call.=FALSE)
+  }
+  if(p<1){
+    stop("need at least 1 lag.\n",call.=FALSE)
+  }
+  if(p>nrow(mydata)){
+    stop("need more data points than lags.\n",call.=FALSE)
+  }
+  for(i in 1:ncol(mydata)){
+    if(sum(is.na(mydata[,i]))){
+      stop("no missing observations allowed.\n",call.=FALSE)
+    }
+  }
+  #
+  # Errors around prior coefs
+  #
+  # If no prior is provided for the coefficients, set to a random walk (in levels)
+  if(class(coefprior)=="NULL"){
+    coefprior <- c(rep(1,ncol(mydata)))
+  }
+  if(class(coefprior)=="matrix"){
+    if(constant==T){
+      if(length(c(coefprior)) != ((ncol(mydata)*p + 1)*ncol(mydata))){
+        stop("you have opted to give a full prior matrix on Beta. However,\n", "it is not of the appropriate dimensions, which are ",(ncol(mydata)*p + 1)," x ",ncol(mydata),".\n",call.=FALSE)
+      }
+    }else{
+      if(length(c(coefprior)) != ((ncol(mydata)*p)*ncol(mydata))){
+        stop("you have opted to give a full prior matrix on Beta. However,\n", "it is not of the appropriate dimensions, which are ",(ncol(mydata)*p)," x ",ncol(mydata),".\n",call.=FALSE)
+      }
+    }
+  }
+  if(class(coefprior)=="numeric"){
+    if(length(coefprior) != ncol(mydata)){
+      stop("you have opted to give a prior on the first-own lags in Beta. However,\n", "it is not of the appropriate dimensions; please provide a numeric vector of length ",ncol(mydata),".\n",call.=FALSE)
+    }
+  }else{coefprior <- coefprior}
+  #
+  #
+  # Some possible errors around hyperparameters and such
+  if(VType != 1 && VType != 2){
+    stop("VType can only be 1 or 2.\n",call.=FALSE)
+  }
+  if(decay != "H" && decay != "G"){
+    stop("decay must be H or G (and quotation marks both sides!).\n",call.=FALSE)
+  }
+  if(HP4 <= 0){
+    stop("HP4 must be greater than zero.\n",call.=FALSE)
+  }
+  #
+  return=list(coefprior=coefprior)
+}
+
+.bvarmdata <- function(mydata,p,constant){
+  Tr <- as.numeric(dim(mydata)[1])
+  M <- as.numeric(dim(mydata)[2])
+  #
+  Yraw <- as.matrix(mydata,ncol=M)
+  #
+  X <- embed(Yraw,p+1); X <- X[,(M+1):ncol(X)]
+  if(constant == TRUE){X<-cbind(rep(1,(Tr-p)),X)}
+  #
+  K <- as.numeric(dim(X)[2])
+  Z <- kronecker(diag(M),X)
+  Y <- Yraw[(p+1):nrow(Yraw),]
+  Tp <- Tr - p
+  #
+  return=list(Y=Y,X=X,Z=Z,M=M,K=K,Yraw=Yraw,Tp=Tp)
+}
+
+.bvarmprior <- function(Y,X,p,M,K,Yraw,Tp,coefprior,constant,VType,decay,HP1,HP2,HP3,HP4){
+  Beta <- solve.qr(qr(as.matrix(X)),as.matrix(Y))
+  alpha <- c(Beta)
+  #
+  if(class(coefprior)=="numeric"){
+    if(constant==T){
+      BPr<-rbind(rep(0,M),diag(M),matrix(rep(0,(p-1)*M*M),nrow=(p-1)*M,ncol=M))
+    }else{
+      BPr<-rbind(diag(M),matrix(rep(0,(p-1)*M*M),nrow=(p-1)*M,ncol=M))
+    }
+    for(i in 1:M){BPr[(i+1),i]<-coefprior[i]}
+  }else{BPr <- coefprior}
+  aPr <- c(BPr)
+  #
+  Sigma<-rep(0,M)
+  #
+  for(i in 1:M){
+    XAR <- embed(Yraw[,i],p+1)
+    YAR <- matrix(XAR[,1])
+    XAR <- XAR[,2:ncol(XAR)]; XAR <- as.matrix(XAR,ncol=p); 
+    if(constant==T){ XAR <- cbind(as.matrix(rep(1,nrow(YAR)),ncol=1),XAR) }
+    #
+    alphaAR <- solve.qr(qr(XAR),YAR)
+    if(constant==T){
+      Sigma[i] <- (1/(Tp-p-1))*t(YAR - XAR%*%alphaAR)%*%(YAR - XAR%*%alphaAR)
+    }else{
+      Sigma[i] <- (1/(Tp-p))*t(YAR - XAR%*%alphaAR)%*%(YAR - XAR%*%alphaAR)
+    }
+  }
+  #
+  # Prior covariance matrix of alpha
+  #
+  BVPr <- matrix(0,nrow=K,ncol=M)
+  #
+  if(VType==1){
+    Cons <- 0
+    if(constant==TRUE){
+      BVPr[1,] <- Sigma*HP3
+      Cons <- 1
+    }
+    #
+    for(i in 1:p){
+      #j is the column, k is the row
+      for(j in 1:M){
+        for(k in 1:M){
+          BVPr[((i-1)*M + k + Cons),j] <- (HP2*Sigma[j])/((i^2)*Sigma[k])
+        }
+        BVPr[((i-1)*M + j + Cons),j] <- HP1/(i^2)
+      }
+    }
+  }else{
+    Cons <- 0
+    if(constant==TRUE){
+      BVPr[1,] <- HP1*HP3
+      Cons <- 1
+    }
+    # Lag decay function
+    if(decay=="H"){
+      LDecay <- function(x,HP4){
+        VDecay <- x^(HP4)
+        return(VDecay)
+      }
+    }else{
+      LDecay <- function(x,HP4){
+        VDecay <- HP4^((-x) + 1)
+        return(VDecay)
+      }
+    }
+    #
+    for(i in 1:p){
+      #j is the column, k is the row
+      for(j in 1:M){
+        for(k in 1:M){
+          BVPr[((i-1)*M + k + Cons),j] <- HP1*(HP2*Sigma[k])/((LDecay(i,HP4))*Sigma[j])
+        }
+        BVPr[((i-1)*M + j + Cons),j] <- HP1/(LDecay(i,HP4))
+      }
+    }
+  }
+  #
+  #
+  #
+  BVPr <- c(BVPr)
+  BVPr <- diag(BVPr)
+  #
+  Sigma <- c(Sigma)
+  Sigma <- diag(Sigma)
+  #
+  return=list(aPr=aPr,alpha=alpha,BVPr=BVPr,Sigma=Sigma)
+}
+
+.bvarmreplications <- function(Y,Z,aPr,alpha,BVPr,Sigma,K,M,p,constant,keep,burnin,irf.periods){
+  #
+  ImpStore <- 0
+  #
+  SigmaO<-Sigma
+  shock<-chol(Sigma)
+  #
+  Sigma <- kronecker(solve(Sigma),diag(nrow(Y)))
+  #
+  message('Starting Gibbs C++, ', date(),'.', sep="")
+  RepsRun <- .Call("MBVARReps", Sigma,as.matrix(Z),as.matrix(Y),matrix(aPr,ncol=1),BVPr,M,K,burnin,keep, PACKAGE = "BMR", DUP = FALSE)
+  message('C++ reps finished, ', date(),'. Now generating IRFs.', sep="")
+  #
+  kcons <- 0; if(constant==T){kcons<-1}
+  ImpStore <- .Call("MBVARIRFs", shock,M,K,kcons,keep,irf.periods,RepsRun$Beta, PACKAGE = "BMR", DUP = FALSE)
+  ImpStore <- ImpStore$ImpStore
+  ImpStore2 <- array(NA,dim=c(M,M,irf.periods,keep))
+  for(i in 1:keep){
+    ImpStore2[,,,i] <- ImpStore[,,((i-1)*irf.periods+1):(i*irf.periods)]
+  }
+  #
+  BetaMean <- apply(RepsRun$Beta,c(1,2),mean)
+  #
+  BNames <- character(length=nrow(BetaMean))
+  kcon <- 1; if(constant==T){BNames[1] <- "Constant"};if(constant==T){kcon<-2}
+  for(i in 1:p){  
+    for(j in 1:M){
+      BNames[kcon] <- paste("Eq ",j,", lag ",i,sep="")
+      kcon <- kcon + 1
+    }
+  }
+  rownames(BetaMean) <- BNames
+  if(class(colnames(Y)) == "character"){
+    colnames(BetaMean) <- colnames(Y)
+  }
+  #
+  ImpSorted <- apply(ImpStore2,c(3,1,2),sort)
+  #
+  ImpSorted <- aperm(ImpSorted,c(2,3,1,4))
+  #
+  return=list(Beta=BetaMean,BetaDraws=RepsRun$Beta,Sigma=SigmaO,IRFs=ImpSorted)
+}
---
+++ src/dsgevarc.cpp
@@ -0,0 +1,452 @@
+/*################################################################################
+  ##
+  ##   R package BMR by Keith O'Hara Copyright (C) 2011, 2012, 2013, 2014, 2015
+  ##   This file is part of the R package BMR.
+  ##
+  ##   The R package BMR is free software: you can redistribute it and/or modify
+  ##   it under the terms of the GNU General Public License as published by
+  ##   the Free Software Foundation, either version 3 of the License, or
+  ##   (at your option) any later version.
+  ##
+  ##   The R package BMR is distributed in the hope that it will be useful,
+  ##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ##   GNU General Public License for more details.
+  ##
+  ################################################################################*/
+
+#include "dsgevarc.h"
+using namespace Rcpp;
+
+SEXP DSGEVARPriorC( SEXP mdsgedata , SEXP mObserveMat , SEXP mObsCons , SEXP mF , SEXP mG , 
+                    SEXP mN , SEXP mshocks , SEXP mR , SEXP mp , SEXP mMaxIter )
+{
+  try {
+    arma::mat dsgedata = as<arma::mat>(mdsgedata);
+    arma::mat ObserveMat = as<arma::mat>(mObserveMat);
+    arma::mat ObsCons = as<arma::mat>(mObsCons);
+    arma::mat F = as<arma::mat>(mF);
+    arma::mat G = as<arma::mat>(mG);
+    arma::mat N = as<arma::mat>(mN);
+    arma::mat shocks = as<arma::mat>(mshocks);
+    arma::mat R = as<arma::mat>(mR);
+    //
+    arma::mat OC2 = ObsCons*trans(ObsCons);
+    //
+    int p = as<int>(mp);
+    //
+    double MaxIter = as<int>(mMaxIter);
+    //
+    int i,j;
+    //
+    arma::mat Q(G.n_rows,G.n_rows);
+    Q.zeros();
+    Q(arma::span(G.n_rows-N.n_rows,G.n_rows-1),arma::span(G.n_rows-N.n_rows,G.n_rows-1)) = shocks;
+    //
+    arma::mat GQG = G*Q*trans(G);
+    //
+    arma::mat SigmaSS = arma::eye(F.n_cols,F.n_cols);
+    //
+    arma::mat SigmaSSOld = GQG;
+    arma::mat SigmaSSNew = GQG;
+    arma::mat IMat = F;
+    //
+    for(j=1;j<=MaxIter;j++){
+      SigmaSSNew = SigmaSSOld + (IMat*SigmaSSOld*trans(IMat));
+      IMat *= IMat;
+      //
+      SigmaSSOld = SigmaSSNew;
+    }
+    SigmaSS = SigmaSSNew;
+    //
+    arma::cube SigmaX(dsgedata.n_cols, dsgedata.n_cols, p+1);
+    SigmaX.zeros();
+    //
+    SigmaX(arma::span(),arma::span(),arma::span(0,0)) = OC2 + trans(ObserveMat)*SigmaSS*ObserveMat + R;
+    //
+    arma::mat Fp = arma::eye(F.n_cols,F.n_cols);
+    for(i=1;i<=p;i++){
+      Fp *= F;
+      SigmaX(arma::span(),arma::span(),arma::span(i,i)) = OC2 + trans(ObserveMat)*Fp*SigmaSS*ObserveMat;
+    }
+    //
+    return Rcpp::List::create(Rcpp::Named("SigmaX") = SigmaX);
+  } catch( std::exception &ex ) {
+    forward_exception_to_r( ex );
+  } catch(...) {
+    ::Rf_error( "BMR: DSGEVAR Prior C++ exception (unknown reason)" );
+  }
+  return R_NilValue;
+}
+
+SEXP DSGEVARLikelihood( SEXP mlogGPR, SEXP mXX, SEXP mGammaYY, SEXP mGammaXY , SEXP mGammaXX , 
+                        SEXP mGammaBarYY , SEXP mGammaBarXY, SEXP mGammaBarXX , 
+                        SEXP mlambda , SEXP mTp , SEXP mM , SEXP mp , SEXP mkcons )
+{
+  try {
+    //
+    float logGPR = as<double>(mlogGPR);
+    float lambda = as<double>(mlambda);
+    float Tp = as<int>(mTp);
+    float M = as<int>(mM);
+    float p = as<int>(mp);
+    int kcons = as<int>(mkcons);
+    //
+    arma::mat XX = as<arma::mat>(mXX);
+    //
+    arma::mat GammaYY = as<arma::mat>(mGammaYY);
+    arma::mat GammaXY = as<arma::mat>(mGammaXY);
+    arma::mat GammaXX = as<arma::mat>(mGammaXX);
+    //
+    arma::mat GammaBarYY = as<arma::mat>(mGammaBarYY);
+    arma::mat GammaBarXY = as<arma::mat>(mGammaBarXY);
+    arma::mat GammaBarXX = as<arma::mat>(mGammaBarXX);
+    //
+    float lambdaT = Tp*lambda;
+    float invlambda = 1/lambda;
+    float M2 = M/2;
+    //
+    arma::mat invGammaXX = arma::inv_sympd(GammaXX);
+    arma::mat invGammaBarXX = arma::inv_sympd(GammaBarXX);
+    arma::mat SigmaEpsilon = GammaYY - GammaXY*invGammaXX*trans(GammaXY);
+    arma::mat SigmaHatEpsilon = GammaBarYY - trans(GammaBarXY)*invGammaBarXX*GammaBarXY;
+    //
+    arma::mat invlambdaXX = invlambda*XX;
+    double Term1 = -M2*log(arma::det(GammaXX + invlambdaXX)) + M2*log(arma::det(GammaXX));
+    arma::mat invlambdaSigma = (1 + invlambda)*SigmaHatEpsilon;
+    double Term2 = -((Tp + lambdaT - M*p - kcons)/2)*log(arma::det( invlambdaSigma ));
+    double Term3 = ((lambdaT - M*p - kcons)/2)*log(arma::det(SigmaEpsilon));
+    double logLikelihood = Term1 + Term2 + Term3 + logGPR - (((M*Tp)/2)*log(lambdaT*arma::datum::pi));
+    // (note that this is not the negative of the loglikelihood)
+    return Rcpp::List::create(Rcpp::Named("logLikelihood") = logLikelihood);
+  } catch( std::exception &ex ) {
+    //forward_exception_to_r( ex );
+  } catch(...) {
+    ::Rf_error( "BMR: DSGEVAR Likelihood C++ exception (unknown reason)" );
+  }
+  //return R_NilValue;
+  double LLKerr = -1000000;
+  return Rcpp::List::create(Rcpp::Named("logLikelihood") = LLKerr);
+}
+
+SEXP DSGEVARLikelihoodInf( SEXP mYY , SEXP mXY , SEXP mXX , 
+                           SEXP mGammaYY , SEXP mGammaXY , SEXP mGammaXX , 
+                           SEXP mTp , SEXP mM , SEXP mp )
+{
+  try {
+    //
+    float Tp = as<int>(mTp);
+    float M = as<int>(mM);
+    float p = as<int>(mp);
+    //
+    arma::mat YY = as<arma::mat>(mYY);
+    arma::mat XY = as<arma::mat>(mXY);
+    arma::mat XX = as<arma::mat>(mXX);
+    //
+    arma::mat GammaYY = as<arma::mat>(mGammaYY);
+    arma::mat GammaXY = as<arma::mat>(mGammaXY);
+    arma::mat GammaXX = as<arma::mat>(mGammaXX);
+    //
+    arma::mat invGammaXX = arma::inv_sympd(GammaXX);
+    arma::mat SigmaEpsilon = GammaYY - GammaXY*invGammaXX*trans(GammaXY);
+    arma::mat Beta = invGammaXX*trans(GammaXY);
+    arma::mat SigmaBarEpsilon = YY + trans(Beta)*XX*Beta - trans(Beta)*XY - trans(XY)*Beta;
+    //
+    double logLikelihood = - (((M*Tp)/2)*log(2*arma::datum::pi)) - ((Tp/2)*log(arma::det(SigmaEpsilon))) - ((Tp/2)*arma::trace(arma::inv_sympd(SigmaEpsilon)*SigmaBarEpsilon));
+    // (note that this is not the negative of the loglikelihood)
+    return Rcpp::List::create(Rcpp::Named("logLikelihood") = logLikelihood);
+  } catch( std::exception &ex ) {
+    //forward_exception_to_r( ex );
+  } catch(...) {
+    ::Rf_error( "BMR: DSGEVAR Likelihood C++ exception (unknown reason)" );
+  }
+  //return R_NilValue;
+  double LLKerr = -1000000;
+  return Rcpp::List::create(Rcpp::Named("logLikelihood") = LLKerr);
+}
+
+SEXP DSGEVARReps( SEXP mGammaBarYY , SEXP mGammaBarXY , SEXP mGammaBarXX , SEXP mGXX , 
+                  SEXP mGHXX , SEXP mlambda , SEXP mkreps , SEXP mTp , SEXP mM , SEXP mp ,
+                  SEXP mkcons )
+{
+  try {
+    //
+    float lambda = as<double>(mlambda);
+    int kreps = as<int>(mkreps);
+    float Tp = as<int>(mTp);
+    float M = as<int>(mM);
+    float p = as<int>(mp);
+    int kcons = as<int>(mkcons);
+    //
+    NumericVector GGammaBarYY(mGammaBarYY);
+    NumericVector GGammaBarXY(mGammaBarXY);
+    NumericVector GGammaBarXX(mGammaBarXX);
+    NumericVector GGXX(mGXX);
+    //
+    arma::cube GammaBarYY(GGammaBarYY.begin(), M, M, kreps, false);
+    arma::cube GammaBarXY(GGammaBarXY.begin(), M*p + kcons, M, kreps, false);
+    arma::cube GammaBarXX(GGammaBarXX.begin(), M*p + kcons, M*p + kcons, kreps, false);
+    arma::cube GXX(GGXX.begin(), M*p + kcons, M*p + kcons, kreps, false);
+    //
+    arma::mat GHXX = as<arma::mat>(mGHXX);
+    //
+    arma::cube BetaDraws(M*p + kcons, M, kreps);
+    arma::cube SigmaDraws(M, M, kreps);
+    //
+    int j;
+    //
+    arma::mat GBarYY = GammaBarYY(arma::span(),arma::span(),arma::span(0,0));
+    arma::mat GBarXY = GammaBarXY(arma::span(),arma::span(),arma::span(0,0));
+    arma::mat GBarXX = GammaBarXX(arma::span(),arma::span(),arma::span(0,0));
+    //
+    arma::mat invGBarXX = arma::inv_sympd(GBarXX);
+    arma::mat BetaHat = invGBarXX*GBarXY;
+    arma::mat SigmaEpsilon = GBarYY - trans(GBarXY)*invGBarXX*GBarXY;
+    //
+    float S2 = (1+lambda)*Tp;
+    arma::mat SigmaDraw = S2*SigmaEpsilon;
+    int S3 = (1+lambda)*Tp - M*p - kcons;
+    arma::mat KSigmaRand = arma::randn(SigmaDraw.n_rows,S3);
+    arma::mat A = trans(arma::chol(arma::inv_sympd(SigmaDraw)))*KSigmaRand;
+    SigmaDraw = arma::inv_sympd(A*trans(A));
+    //
+    float B1 = 1/lambda;
+    arma::mat GammaXX = GXX(arma::span(),arma::span(),arma::span(0,0));
+    arma::mat BetaCov = arma::inv_sympd(GammaXX + B1*GHXX);
+    arma::vec KBetaRand = arma::randn(M*(M*p+kcons));
+    arma::colvec VecBetaHat(BetaHat.begin(),BetaHat.size(),false);
+    float B2 = 1/(Tp*lambda);
+    arma::mat VecBetaDraw = VecBetaHat + trans(arma::chol(B2*arma::kron(SigmaEpsilon,BetaCov)))*KBetaRand;
+    arma::mat BetaDraw(VecBetaDraw.begin(),M*p+kcons,M,false);
+    //
+    BetaDraws(arma::span(),arma::span(),arma::span(0,0)) = BetaDraw;
+    SigmaDraws(arma::span(),arma::span(),arma::span(0,0)) = SigmaDraw;
+    //
+    for (j=2;j<=kreps;j++) {
+      GBarYY = GammaBarYY(arma::span(),arma::span(),arma::span(j-1,j-1));
+      GBarXY = GammaBarXY(arma::span(),arma::span(),arma::span(j-1,j-1));
+      GBarXX = GammaBarXX(arma::span(),arma::span(),arma::span(j-1,j-1));
+      //
+      invGBarXX = arma::inv_sympd(GBarXX);
+      BetaHat = invGBarXX*GBarXY;
+      SigmaEpsilon = GBarYY - trans(GBarXY)*invGBarXX*GBarXY;
+      //
+      SigmaDraw = S2*SigmaEpsilon;
+      KSigmaRand = arma::randn(SigmaDraw.n_rows,S3);
+      A = trans(arma::chol(arma::inv(SigmaDraw)))*KSigmaRand;
+      SigmaDraw = arma::inv_sympd(A*trans(A));
+      //
+      GammaXX = GXX(arma::span(),arma::span(),arma::span(j-1,j-1));
+      BetaCov = arma::inv_sympd(GammaXX + B1*GHXX);
+      KBetaRand = arma::randn(M*(M*p+kcons));
+      arma::colvec VecBetaHat(BetaHat.begin(),BetaHat.size(),false);
+      VecBetaDraw = VecBetaHat + trans(arma::chol(B2*arma::kron(SigmaEpsilon,BetaCov)))*KBetaRand;
+      arma::mat BetaDraw(VecBetaDraw.begin(),M*p+kcons,M,false);
+      //
+      BetaDraws(arma::span(),arma::span(),arma::span(j-1,j-1)) = BetaDraw;
+      SigmaDraws(arma::span(),arma::span(),arma::span(j-1,j-1)) = SigmaDraw;
+    }
+    //
+    return Rcpp::List::create(Rcpp::Named("Beta") = BetaDraws,Rcpp::Named("Sigma") = SigmaDraws);
+  } catch( std::exception &ex ) {
+    forward_exception_to_r( ex );
+  } catch(...) {
+    ::Rf_error( "BMR: DSGEVAR Gibbs C++ exception (unknown reason)" );
+  }
+  return R_NilValue;
+}
+
+SEXP DSGEVARRepsInf( SEXP mGammaBarYY , SEXP mGammaBarXY , SEXP mGammaBarXX , SEXP mlambda , 
+                     SEXP mkreps , SEXP mTp , SEXP mM , SEXP mp , SEXP mkcons )
+{
+  try {
+    //
+    float lambda = as<double>(mlambda);
+    int kreps = as<int>(mkreps);
+    float Tp = as<int>(mTp);
+    float M = as<int>(mM);
+    float p = as<int>(mp);
+    int kcons = as<int>(mkcons);
+    //
+    NumericVector GGammaBarYY(mGammaBarYY);
+    NumericVector GGammaBarXY(mGammaBarXY);
+    NumericVector GGammaBarXX(mGammaBarXX);
+    //
+    arma::cube GammaBarYY(GGammaBarYY.begin(), M, M, kreps, false);
+    arma::cube GammaBarXY(GGammaBarXY.begin(), M*p + kcons, M, kreps, false);
+    arma::cube GammaBarXX(GGammaBarXX.begin(), M*p + kcons, M*p + kcons, kreps, false);
+    //
+    arma::cube BetaDraws(M*p + kcons, M, kreps);
+    arma::cube SigmaDraws(M, M, kreps);
+    //
+    int j;
+    //
+    arma::mat GBarYY = GammaBarYY(arma::span(),arma::span(),arma::span(0,0));
+    arma::mat GBarXY = GammaBarXY(arma::span(),arma::span(),arma::span(0,0));
+    arma::mat GBarXX = GammaBarXX(arma::span(),arma::span(),arma::span(0,0));
+    //
+    arma::mat invGBarXX = arma::inv_sympd(GBarXX);
+    arma::mat BetaHat = invGBarXX*GBarXY;
+    arma::mat SigmaEpsilon = GBarYY - trans(GBarXY)*invGBarXX*GBarXY;
+    //
+    BetaDraws(arma::span(),arma::span(),arma::span(0,0)) = BetaHat;
+    SigmaDraws(arma::span(),arma::span(),arma::span(0,0)) = SigmaEpsilon;
+    //
+    for (j=2;j<=kreps;j++) {
+      GBarYY = GammaBarYY(arma::span(),arma::span(),arma::span(j-1,j-1));
+      GBarXY = GammaBarXY(arma::span(),arma::span(),arma::span(j-1,j-1));
+      GBarXX = GammaBarXX(arma::span(),arma::span(),arma::span(j-1,j-1));
+      //
+      invGBarXX = arma::inv_sympd(GBarXX);
+      BetaHat = invGBarXX*GBarXY;
+      SigmaEpsilon = GBarYY - trans(GBarXY)*invGBarXX*GBarXY;
+      //
+      BetaDraws(arma::span(),arma::span(),arma::span(j-1,j-1)) = BetaHat;
+      SigmaDraws(arma::span(),arma::span(),arma::span(j-1,j-1)) = SigmaEpsilon;
+    }
+    //
+    return Rcpp::List::create(Rcpp::Named("Beta") = BetaDraws,Rcpp::Named("Sigma") = SigmaDraws);
+  } catch( std::exception &ex ) {
+    forward_exception_to_r( ex );
+  } catch(...) {
+    ::Rf_error( "BMR: DSGEVAR Gibbs C++ exception (unknown reason)" );
+  }
+  return R_NilValue;
+}
+
+SEXP DSGEVARIRFs( SEXP mM , SEXP mK , SEXP mkcons , SEXP mkreps , SEXP mirfperiods , 
+                  SEXP mBDs , SEXP mSDs , SEXP mIRF )
+{
+  try {
+    int M = as<int>(mM);
+    int K = as<int>(mK);
+    int kcons = as<int>(mkcons);
+    int kreps = as<int>(mkreps);
+    int irfperiods = as<int>(mirfperiods);
+    //
+    NumericVector IRFArray(mIRF);
+    NumericVector BDArray(mBDs);
+    NumericVector SDArray(mSDs);
+    arma::cube ImpStore(IRFArray.begin(), M, M, irfperiods*kreps, false);
+    arma::cube BetaDraws(BDArray.begin(), K, M, kreps, false);
+    arma::cube SigmaDraws(SDArray.begin(), M, M, kreps, false);
+    //
+    int i,j;
+    //
+    arma::mat BetaT(K-kcons,M);
+    BetaT.zeros();
+    arma::mat shock(M,M);
+    shock.zeros();
+    arma::mat ShockO(K-M-kcons,M);
+    ShockO.zeros();
+    arma::mat ShockB(K-kcons,M);
+    ShockB.zeros();
+    arma::mat Pow(M,M);
+    Pow.zeros();
+    //
+    for (j=1;j<=kreps;j++) {
+      BetaT = BetaDraws(arma::span(kcons,K-1),arma::span(),arma::span(j-1,j-1));
+      BetaT = trans(BetaT);
+      shock = SigmaDraws(arma::span(),arma::span(),arma::span(j-1,j-1));
+      ShockB.zeros();
+      ShockB(arma::span(0,M-1),arma::span()) = shock;
+      ImpStore(arma::span(),arma::span(),arma::span((j-1)*irfperiods,(j-1)*irfperiods)) = shock;
+      for (i=2;i<=irfperiods;i++){
+        Pow = BetaT*ShockB;
+        ImpStore(arma::span(),arma::span(),arma::span((j-1)*irfperiods + (i-1),(j-1)*irfperiods + (i-1))) = Pow;
+        if (K > M+kcons){
+          ShockB(arma::span(M,K-1-kcons),arma::span()) = ShockB(arma::span(0,K-M-1-kcons),arma::span());
+        }
+        ShockB(arma::span(0,M-1),arma::span()) = Pow;
+      }
+    }
+    //
+    return Rcpp::List::create(Rcpp::Named("ImpStore") = ImpStore);
+  } catch( std::exception &ex ) {
+    forward_exception_to_r( ex );
+  } catch(...) {
+    ::Rf_error( "BMR: DSGEVAR IRF C++ exception (unknown reason)" );
+  }
+  return R_NilValue;
+}
+
+SEXP dsgevarforecast( SEXP mY0 , SEXP mM , SEXP mp , SEXP mK , SEXP mkcons ,
+                      SEXP mruns , SEXP mperiods , SEXP minclshocks , SEXP mBDs , SEXP mSDs )
+{
+  try {
+    int M = as<int>(mM);
+    int p = as<int>(mp);
+    int K = as<int>(mK);
+    int kcons = as<int>(mkcons);
+    int runs = as<int>(mruns);
+    int periods = as<int>(mperiods);
+    int inclshocks = as<int>(minclshocks);
+    //
+    arma::mat Y0 = as<arma::mat>(mY0);
+    //
+    NumericVector BDArray(mBDs);
+    NumericVector SDArray(mSDs);
+    arma::cube BetaDraws(BDArray.begin(), K, M, runs, false);
+    arma::cube SigmaDraws(SDArray.begin(), M, M, runs, false);
+    //
+    arma::cube Forecasts(periods, M, runs);
+    Forecasts.zeros();
+    //
+    arma::mat BT = BetaDraws(arma::span(),arma::span(),arma::span(0,0));
+    arma::mat ST = SigmaDraws(arma::span(),arma::span(),arma::span(0,0));
+    arma::mat Shock = trans(arma::chol(ST));
+    //
+    arma::mat fY(periods,M);
+    fY.zeros();
+    arma::mat kY = Y0;
+    arma::vec Krand = arma::randn(M);
+    arma::mat jshock(1,M);
+    jshock.zeros();
+    //
+    int i,j;
+    //
+    if(inclshocks > 0){
+      for (i=1;i<=runs;i++) {
+        BT = BetaDraws(arma::span(),arma::span(),arma::span(i-1,i-1));
+        ST = SigmaDraws(arma::span(),arma::span(),arma::span(i-1,i-1));
+        Shock = trans(arma::chol(ST));
+        fY.zeros();
+        kY = Y0;
+        for (j=1;j<=periods;j++) {
+          Krand = arma::randn(M);
+          jshock = trans(Shock*Krand);
+          //
+          fY(arma::span(j-1,j-1),arma::span()) = kY*BT + jshock;
+          if (K > M+kcons){
+            kY(0,arma::span(M+kcons,K-1)) = kY(0,arma::span(kcons,K-M-1)); 
+          }
+          kY(0,arma::span(kcons,M-1+kcons)) = fY(arma::span(j-1,j-1),arma::span());
+        }
+        Forecasts(arma::span(),arma::span(),arma::span(i-1,i-1)) = fY;
+      }
+    }
+    else{
+      for (i=1;i<=runs;i++) {
+        BT = BetaDraws(arma::span(),arma::span(),arma::span(i-1,i-1));
+        ST = SigmaDraws(arma::span(),arma::span(),arma::span(i-1,i-1));
+        fY.zeros();
+        kY = Y0;
+        for (j=1;j<=periods;j++) {
+          fY(arma::span(j-1,j-1),arma::span()) = kY*BT;
+          if (K > M+kcons){
+            kY(0,arma::span(M+kcons,K-1)) = kY(0,arma::span(kcons,K-M-1)); 
+          }
+          kY(0,arma::span(kcons,M-1+kcons)) = fY(arma::span(j-1,j-1),arma::span());
+        }
+        Forecasts(arma::span(),arma::span(),arma::span(i-1,i-1)) = fY;
+      }
+    }
+    //
+    return Rcpp::List::create(Rcpp::Named("Forecasts") = Forecasts);
+  } catch( std::exception &ex ) {
+    forward_exception_to_r( ex );
+  } catch(...) {
+    ::Rf_error( "BMR: DSGEVAR Forecast C++ exception (unknown reason)" );
+  }
+  return R_NilValue;
+}
