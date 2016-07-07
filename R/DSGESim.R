51c1935204dcac13338e80c22771fbfbae167051
Keith O'Hara <kthohr@users.noreply.github.com>
2016-07-07 12:34:41 -0400

    formatting

--- R/DSGESim.R
+++ R/DSGESim.R
@@ -18,57 +18,57 @@
 # 07/20/2015
 
 DSGESim.SDSGE <- function(obj,shocks.cov,sim.periods,burnin=NULL,seedval=1122,hpfiltered=FALSE,lambda=1600,...){
-  results <- .dsgesimulation(obj,shocks.cov,sim.periods,burnin,seedval,hpfiltered,lambda)
-  return(results)
+    results <- .dsgesimulation(obj,shocks.cov,sim.periods,burnin,seedval,hpfiltered,lambda)
+    return(results)
 }
 
 DSGESim.gensys <- function(obj,shocks.cov,sim.periods,burnin=NULL,seedval=1122,hpfiltered=FALSE,lambda=1600,...){
-  results <- .dsgesimulation(obj,shocks.cov,sim.periods,burnin,seedval,hpfiltered,lambda)
-  return(results)
+    results <- .dsgesimulation(obj,shocks.cov,sim.periods,burnin,seedval,hpfiltered,lambda)
+    return(results)
 }
 
 DSGESim.uhlig <- function(obj,shocks.cov,sim.periods,burnin=NULL,seedval=1122,hpfiltered=FALSE,lambda=1600,...){
-  results <- .dsgesimulation(obj,shocks.cov,sim.periods,burnin,seedval,hpfiltered,lambda)
-  return(results)
+    results <- .dsgesimulation(obj,shocks.cov,sim.periods,burnin,seedval,hpfiltered,lambda)
+    return(results)
 }
 
 .dsgesimulation <- function(obj,shocks.cov,sim.periods,burnin=NULL,seedval,hpfiltered=FALSE,lambda=1600){
-  #
-  StateMats <- statespace(obj)
-  F <- StateMats$F; G <- StateMats$G
-  #
-  nShocks <- ncol(G)
-  #
-  if(class(burnin) != "numeric"){
-    burnin <- ceiling(0.5*sim.periods)
-  }
-  #
-  DSim <- matrix(0,nrow=(sim.periods+burnin),ncol=ncol(F))
-  #
-  set.seed(seedval)
-  #
-  A <- t(chol(shocks.cov))
-  krun <- (sim.periods + burnin)*nShocks
-  samp <- matrix(rnorm(krun),ncol=nShocks)
-  shocks <- t(samp%*%t(A))
-  #
-  DSim[1,] <- t(G%*%shocks[,1])
-  for(i in 2:(burnin+sim.periods)){
-    DSim[i,] <- DSim[i-1,]%*%t(F) + t(G%*%shocks[,i])
-  }
-  #
-  DSim <- DSim[(burnin+1):(burnin+sim.periods),]
-  #
-  if(hpfiltered==TRUE){
-    hpfilterq <- function(x,lambda=1600){
-      eye <- diag(length(x))
-      result <- solve(eye+lambda*crossprod(diff(eye,lag=1,d=2)),x)
-      return(result)
+    #
+    StateMats <- statespace(obj)
+    F <- StateMats$F; G <- StateMats$G
+    #
+    nShocks <- ncol(G)
+    #
+    if(class(burnin) != "numeric"){
+        burnin <- ceiling(0.5*sim.periods)
     }
-    for(j in 1:ncol(DSim)){
-      DSim[,j]<-hpfilterq(DSim[,j])
+    #
+    DSim <- matrix(0,nrow=(sim.periods+burnin),ncol=ncol(F))
+    #
+    set.seed(seedval)
+    #
+    A <- t(chol(shocks.cov))
+    krun <- (sim.periods + burnin)*nShocks
+    samp <- matrix(rnorm(krun),ncol=nShocks)
+    shocks <- t(samp%*%t(A))
+    #
+    DSim[1,] <- t(G%*%shocks[,1])
+    for(i in 2:(burnin+sim.periods)){
+        DSim[i,] <- DSim[i-1,]%*%t(F) + t(G%*%shocks[,i])
     }
-  }
-  #
-  return(DSim)
+    #
+    DSim <- DSim[(burnin+1):(burnin+sim.periods),]
+    #
+    if(hpfiltered==TRUE){
+        hpfilterq <- function(x,lambda=1600){
+            eye <- diag(length(x))
+            result <- solve(eye+lambda*crossprod(diff(eye,lag=1,d=2)),x)
+            return(result)
+        }
+        for(j in 1:ncol(DSim)){
+            DSim[,j]<-hpfilterq(DSim[,j])
+        }
+    }
+    #
+    return(DSim)
 }
